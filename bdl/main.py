e97f594e2aa2ff963fdba6f27ec880ef2b972399
amrojjeh <amrojjeh@gmail.com>
2020-04-25 04:46:29 -0500

    Added comments

--- bdl/main.py
+++ bdl/main.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, g, session, render_template
+from flask import Blueprint, g, session, render_template, request, redirect, flash, url_for
+from werkzeug.security import check_password_hash, generate_password_hash
 
 bp = Blueprint("main", __name__, url_prefix="")
 
@@ -10,11 +11,30 @@ def index():
 def login():
 	return render_template("main/login.html")
 
-@bp.route("/register")
+@bp.route("/register", methods=("GET", "POST"))
 def register():
-	return render_template("main/register.html")
+	if request.method == "GET":
+		return render_template("main/register.html")
+	code = request.form["code"]
+	username = request.form["username"]
+	password = request.form["password"]
+	email = request.form["email"]
+
+	# Check if code is in db
+	# Remove code
+
+	# Check if username is unique
+
+	# Check if email is valid
+
+	# Insert user
+
+	# Save cookie
+
+	# Make user global
+
+	return redirect(url_for("main.index"))
 
 @bp.route("/forgotPass")
 def forgotPass():
 	return render_template("main/forgotPass.html")
-
