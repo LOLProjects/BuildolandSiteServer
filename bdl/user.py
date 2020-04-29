6f8fa2819353c456c05341b9b973f9401b0e2679
amrojjeh <amrojjeh@gmail.com>
2020-04-28 19:55:51 -0500

    Added more for the change page

--- bdl/user.py
+++ bdl/user.py
@@ -20,14 +20,15 @@ class RegisterResult:
 	USERNAME_TAKEN = 1
 	INVALID_EMAIL = 2
 	INVALID_CODE = 3
+	INVALID_USERNAME = 4
 
 # generates a new UUIDv4 for a new user
 def new_uuid():
 	db = get_db()
-	# id = getattr(g, "test_user_id", None)
-	# if id is None:
-	id = uuid4()
-	while (db.execute("SELECT id FROM user WHERE id=?", (id.bytes,)).fetchone() is not None):
+	id = g.get("test_user_id")
+	if not id:
+		id = uuid4()
+	while (db.execute("SELECT id FROM user WHERE id=?", (id.bytes,)).fetchone()):
 		id = uuid4()
 	return id
 
@@ -60,6 +61,9 @@ def unique_username(username):
 def valid_email(email):
 	return re.match(r"^[_a-z0-9-]+(\.[_a-z0-9-]+)*(\+[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9]+)*\.([a-z]{2,4})$", email) is not None
 
+def valid_username(username):
+	return re.match(r"[_0-9A-Za-z-]+$", username) is not None
+
 def add_user(email, username, hashed_password, code_used=None):
 	user_id = new_uuid().bytes
 	db = get_db()
@@ -68,8 +72,12 @@ def add_user(email, username, hashed_password, code_used=None):
 	db.commit()
 	return User(user_id, email, username, hashed_password, code_used)
 
-def register_user(email, username, password, code=None):
+def register_user(email, username, password, code=""):
 	"""Registers the user, returns (errorCode, user)"""
+
+	if (not valid_username(username)):
+		return (RegisterResult.INVALID_USERNAME, None)
+
 	if (not unique_username(username)):
 		return (RegisterResult.USERNAME_TAKEN, None)
 
@@ -79,7 +87,7 @@ def register_user(email, username, password, code=None):
 	# TODO: Send a verification email
 	# TODO: Start a test email server
 
-	if (code is not None):
+	if (code):
 		if (not valid_code(code)):
 			return (RegisterResult.INVALID_CODE, None)
 
@@ -88,3 +96,16 @@ def register_user(email, username, password, code=None):
 	hashed_password = generate_password_hash(password)
 	user = add_user(email, username, hashed_password, code)
 	return (RegisterResult.SUCCESS, user)
+
+def change_user(user, username="", password=""):
+	db = get_db()
+	if (password):
+		password = generate_password_hash(password)
+	else:
+		password = user.hashed_password
+
+	if (not username):
+		username = g.user.username
+
+	db.execute("UPDATE user SET username=?, password=? WHERE id=?", (username, password, user.id))
+	db.commit()
