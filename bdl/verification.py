c9524002c955f2d1b4cfdf8a6201f1397c0227cb
amrojjeh <amrojjeh@gmail.com>
2020-05-06 10:19:23 -0500

    Send verification added

---
+++ bdl/verification.py
@@ -0,0 +1,15 @@
+from email.message import EmailMessage
+from base64 import b64encode
+
+from flask import g, current_app, url_for
+from bdl.email import get_smtp
+
+# TODO: Get an actual email service with an actual address
+def send_verification():
+	msg = EmailMessage()
+	msg["Subject"] = "Buildoland Verification"
+	msg["From"] = f"Buildoland <{current_app.config['EMAIL']}>"
+	msg["To"] = g.user.email
+	url = current_app.config["DNS"] + url_for("auth.verify", token=b64encode(g.user.verif_token))
+	msg.set_content(f"Click to the following to verify your email address: {url}")
+	get_smtp().send_message(msg)
