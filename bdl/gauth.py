c9cc32b22fd09acc7d25503a3cc00b9a85153b70
amrojjeh <amrojjeh@gmail.com>
2020-05-17 13:13:22 -0500

    Made some tests

--- bdl/gauth.py
+++ bdl/gauth.py
@@ -1,3 +1,11 @@
 # Gauth because it's game auth
 
+from flask import Blueprint, request
+from uuid import uuid4
+
+bp = Blueprint("gauth", __name__, url_prefix="/gauth")
+
+@bp.route("/start", methods=("POST",))
+def start():
+	return "aa44f915-50d2-4d19-bc87-f6dfcd4a1b5e".replace("-", "")
 
